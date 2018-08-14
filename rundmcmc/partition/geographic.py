f687265671a6cd3e7aa5b33dc1c1e5cd0c34f9fd
Max Hully <max.hully@gmail.com>
2018-08-14 16:58:18 -0400

    Add validation to GeographicPartition

--- rundmcmc/partition/geographic.py
+++ rundmcmc/partition/geographic.py
@@ -1,3 +1,5 @@
+import warnings
+
 from ..updaters import (boundary_nodes, cut_edges, cut_edges_by_part,
                         exterior_boundaries, interior_boundaries, perimeters)
 from .updating import UpdatingPartition
@@ -16,10 +18,27 @@ class GeographicPartition(UpdatingPartition):
 
     def __init__(self, graph=None, assignment=None, updaters=None,
                  parent=None, flips=None):
-        # TODO: Validate here that the graph has the right attributes
+        validate_graph(graph)
+
         if updaters:
             # Here, .update() is the dictionary method, not
             # anything to do with our notion of updaters.
             updaters.update(self.default_updaters)
         super().__init__(graph=graph, assignment=assignment, updaters=updaters,
                          parent=parent, flips=flips)
+
+
+def validate_graph(self, graph):
+    edges_are_valid = all('shared_perim' in graph.edges[edge]
+                            for edge in graph.edges)
+    if not edges_are_valid:
+        raise TypeError('The provided graph does not have "shared_perim" edge attributes.')
+
+    for node in graph.nodes:
+        if graph.nodes[node]['boundary_node'] and 'boundary_perim' not in graph.nodes[node]:
+            raise TypeError('The provided graph has invalid boundary data.')
+
+    if all(not graph.nodes[node]['boundary_node'] for node in graph.nodes):
+        warnings.warn('The provided graph contains no boundary nodes')
+
+    return True
